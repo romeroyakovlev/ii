aa373d07b1f681b0a21199c606fc374cbb0f7c84
user <user@deb64>
2014-04-04 12:16:54 +1100

    urlsafe-for-mktoss

--- tools/client-toss/mktoss.py
+++ tools/client-toss/mktoss.py
@@ -2,5 +2,5 @@ import sys, base64
 
 for f in sys.argv[1:]:
     tx = open(f).read()
-    ctx = base64.b64encode( (tx) )
+    ctx = base64.urlsafe_b64encode( (tx) )
     open(f + '.toss','w').write(ctx)
